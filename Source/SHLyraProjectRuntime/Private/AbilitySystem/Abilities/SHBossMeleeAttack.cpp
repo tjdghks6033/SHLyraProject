987173a5b024182d9b50b1c19e5c6476ab99bfe8
tjdghks6033 <59431045+tjdghks6033@users.noreply.github.com>
2026-05-30 16:20:02 +0900

    refactor: USHBossMeleeAttack를 USHMeleeAttackBase 상속으로 전환
    멜리와 동일하게 베이스 위임. 스켈레탈 소켓(hand_r) 단일 위치 오버랩 판정만 ComputeHitTrace로 구현.

--- Source/SHLyraProjectRuntime/Private/AbilitySystem/Abilities/SHBossMeleeAttack.cpp
+++ Source/SHLyraProjectRuntime/Private/AbilitySystem/Abilities/SHBossMeleeAttack.cpp
@@ -2,13 +2,8 @@
 
 #include "AbilitySystem/Abilities/SHBossMeleeAttack.h"
 
-#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
-#include "Abilities/Tasks/AbilityTask_WaitGameplayEvent.h"
-#include "AbilitySystemComponent.h"
-#include "AbilitySystemBlueprintLibrary.h"
 #include "Components/SkeletalMeshComponent.h"
 #include "GameFramework/Character.h"
-#include "Kismet/KismetSystemLibrary.h"
 #include "NativeGameplayTags.h"
 
 // 보스 근접 히트 판정 시점 태그. 몽타주에 AnimNotify_GameplayEvent 로 추가한다.
@@ -21,146 +16,40 @@ USHBossMeleeAttack::USHBossMeleeAttack(const FObjectInitializer& ObjectInitializ
 	bLockMovementOnActivate = true;
 }
 
-void USHBossMeleeAttack::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
-	const FGameplayAbilityActorInfo* ActorInfo,
-	const FGameplayAbilityActivationInfo ActivationInfo,
-	const FGameplayEventData* TriggerEventData)
+FGameplayTag USHBossMeleeAttack::GetHitDetectTag() const
 {
-	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
-
-	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
-	{
-		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
-		return;
-	}
-
-	if (!AttackMontage)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("USHBossMeleeAttack: AttackMontage가 설정되지 않았습니다."));
-		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
-		return;
-	}
-
-	UAbilityTask_PlayMontageAndWait* MontageTask =
-		UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(
-			this, NAME_None, AttackMontage, 1.0f, NAME_None, true);
-
-	MontageTask->OnCompleted.AddDynamic(this, &USHBossMeleeAttack::OnMontageCompleted);
-	MontageTask->OnBlendOut.AddDynamic(this, &USHBossMeleeAttack::OnMontageCompleted);
-	MontageTask->OnCancelled.AddDynamic(this, &USHBossMeleeAttack::OnMontageCancelled);
-	MontageTask->OnInterrupted.AddDynamic(this, &USHBossMeleeAttack::OnMontageCancelled);
-	MontageTask->ReadyForActivation();
-
-	UAbilityTask_WaitGameplayEvent* WaitEventTask =
-		UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(
-			this, TAG_Event_SH_Boss_HitDetect, nullptr, false, true);
-
-	WaitEventTask->EventReceived.AddDynamic(this, &USHBossMeleeAttack::OnGameplayEventReceived);
-	WaitEventTask->ReadyForActivation();
+	return TAG_Event_SH_Boss_HitDetect;
 }
 
-void USHBossMeleeAttack::EndAbility(const FGameplayAbilitySpecHandle Handle,
-	const FGameplayAbilityActorInfo* ActorInfo,
-	const FGameplayAbilityActivationInfo ActivationInfo,
-	bool bReplicateEndAbility, bool bWasCancelled)
-{
-	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
-}
-
-void USHBossMeleeAttack::OnMontageCompleted()
-{
-	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
-}
-
-void USHBossMeleeAttack::OnMontageCancelled()
-{
-	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
-}
-
-void USHBossMeleeAttack::OnGameplayEventReceived(FGameplayEventData Payload)
-{
-	if (HasAuthority(&CurrentActivationInfo))
-	{
-		PerformBossHit(CurrentActorInfo);
-	}
-}
-
-void USHBossMeleeAttack::PerformBossHit(const FGameplayAbilityActorInfo* ActorInfo)
+bool USHBossMeleeAttack::ComputeHitTrace(const FGameplayAbilityActorInfo* ActorInfo,
+	FVector& OutStart, FVector& OutEnd, float& OutRadius) const
 {
 	AActor* AvatarActor = ActorInfo->AvatarActor.Get();
-	if (!AvatarActor || !DamageEffect)
+	if (!AvatarActor)
 	{
-		return;
+		return false;
 	}
 
-	UAbilitySystemComponent* InstigatorASC = ActorInfo->AbilitySystemComponent.Get();
-	if (!InstigatorASC)
-	{
-		return;
-	}
+	OutRadius = HitRadius;
 
 	// 보스 메인 스켈레탈 메시의 AttackSocket(hand_r) 위치를 판정 중심으로 사용.
 	// hand_r 소켓은 보스 무기가 부착된 위치이므로 공격 범위와 자연스럽게 일치한다.
-	FVector TraceOrigin;
-
 	ACharacter* BossCharacter = Cast<ACharacter>(AvatarActor);
 	USkeletalMeshComponent* SkelMesh = BossCharacter ? BossCharacter->GetMesh() : nullptr;
 
 	if (SkelMesh && SkelMesh->DoesSocketExist(AttackSocketName))
 	{
-		TraceOrigin = SkelMesh->GetSocketLocation(AttackSocketName);
+		OutStart = SkelMesh->GetSocketLocation(AttackSocketName);
 	}
 	else
 	{
 		// 폴백: 캐릭터 중심(60cm 높이)에서 전방 FallbackRange 절반 거리
-		TraceOrigin = AvatarActor->GetActorLocation()
+		OutStart = AvatarActor->GetActorLocation()
 			+ FVector(0.f, 0.f, 60.f)
 			+ AvatarActor->GetActorForwardVector() * (FallbackRange * 0.5f);
 	}
 
-	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes;
-	ObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECC_Pawn));
-
-	const EDrawDebugTrace::Type DebugType = bDrawDebugTrace
-		? EDrawDebugTrace::ForDuration
-		: EDrawDebugTrace::None;
-
 	// Start == End: 단일 위치의 구체 오버랩
-	TArray<FHitResult> HitResults;
-	UKismetSystemLibrary::SphereTraceMultiForObjects(
-		AvatarActor,
-		TraceOrigin,
-		TraceOrigin,
-		HitRadius,
-		ObjectTypes,
-		false,
-		TArray<AActor*>(),
-		DebugType,
-		HitResults,
-		true           // bIgnoreSelf
-	);
-
-	TSet<AActor*> DamagedActors;
-
-	for (const FHitResult& Hit : HitResults)
-	{
-		AActor* HitActor = Hit.GetActor();
-		if (!HitActor || DamagedActors.Contains(HitActor))
-		{
-			continue;
-		}
-
-		UAbilitySystemComponent* TargetASC =
-			UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(HitActor);
-
-		if (!TargetASC)
-		{
-			continue;
-		}
-
-		// 데미지 적용. HitResult를 컨텍스트에 실어 USHDamageExecution이 히트 정보를 활용하게 한다.
-		ApplyEffectToTarget(DamageEffect, InstigatorASC, TargetASC, &Hit);
-
-		DamagedActors.Add(HitActor);
-	}
+	OutEnd = OutStart;
+	return true;
 }
--- Source/SHLyraProjectRuntime/Public/AbilitySystem/Abilities/SHBossMeleeAttack.h
+++ Source/SHLyraProjectRuntime/Public/AbilitySystem/Abilities/SHBossMeleeAttack.h
@@ -2,36 +2,23 @@
 
 #pragma once
 
-#include "AbilitySystem/Abilities/SHGameplayAbility.h"
+#include "AbilitySystem/Abilities/SHMeleeAttackBase.h"
 
 #include "SHBossMeleeAttack.generated.h"
-
-class UAnimMontage;
-class UGameplayEffect;
-struct FGameplayEventData;
 
 /**
  * USHBossMeleeAttack
  *
- * 보스 근접 공격 어빌리티.
- *
- * 실행 흐름:
- *   1. ActivateAbility → CommitAbility
- *   2. UAbilityTask_PlayMontageAndWait 로 AttackMontage 재생
- *   3. UAbilityTask_WaitGameplayEvent 로 'Event.SH.Boss.HitDetect' 대기
- *      → AnimNotify_GameplayEvent 발화 시 OnGameplayEventReceived 호출
- *   4. PerformBossHit: AttackSocket 중심 SphereTrace → DamageEffect 적용
- *      (소켓이 없으면 캐릭터 전방 구체 트레이스로 폴백)
- *   5. 몽타주 완료/취소 → EndAbility
+ * 보스 근접 공격. 공통 흐름은 USHMeleeAttackBase가 담당하고,
+ * 이 클래스는 스켈레탈 메시 소켓(hand_r) 중심의 단일 위치 구체 오버랩 판정을 제공한다.
+ * 소켓이 없으면 캐릭터 전방 구체 트레이스로 폴백한다.
  *
  * BT 연동:
  *   BTTask_SHActivateAbility 가 AbilityTag=Ability.SH.Boss.Melee 로 이 어빌리티를 발동한다.
  *   쿨다운 간격은 BT 의 Wait 노드로 제어한다.
- *
- * 히트 판정은 서버(Authority)에서만 실행된다.
  */
 UCLASS()
-class SHLYRAPROJECTRUNTIME_API USHBossMeleeAttack : public USHGameplayAbility
+class SHLYRAPROJECTRUNTIME_API USHBossMeleeAttack : public USHMeleeAttackBase
 {
 	GENERATED_BODY()
 
@@ -41,44 +28,11 @@ public:
 
 protected:
 
-	//~ UGameplayAbility interface
-	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle,
-		const FGameplayAbilityActorInfo* ActorInfo,
-		const FGameplayAbilityActivationInfo ActivationInfo,
-		const FGameplayEventData* TriggerEventData) override;
-
-	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle,
-		const FGameplayAbilityActorInfo* ActorInfo,
-		const FGameplayAbilityActivationInfo ActivationInfo,
-		bool bReplicateEndAbility, bool bWasCancelled) override;
-	//~ End of UGameplayAbility interface
-
-private:
-
-	UFUNCTION()
-	void OnMontageCompleted();
-
-	UFUNCTION()
-	void OnMontageCancelled();
-
-	// AnimNotify_GameplayEvent 가 'Event.SH.Boss.HitDetect' 를 발화하면 호출됨.
-	UFUNCTION()
-	void OnGameplayEventReceived(FGameplayEventData Payload);
-
-	// AttackSocket 위치를 중심으로 SphereTrace 를 수행하고
-	// 범위 내 대상의 ASC 에 DamageEffect 를 적용한다.
-	void PerformBossHit(const FGameplayAbilityActorInfo* ActorInfo);
-
-protected:
-
-	// 공격 애니메이션 몽타주. GA_SHBossMelee BP 에서 지정한다.
-	// 몽타주에 AnimNotify_GameplayEvent(Event.SH.Boss.HitDetect) 를 추가해야 한다.
-	UPROPERTY(EditDefaultsOnly, Category = "SH|Boss|Melee")
-	TObjectPtr<UAnimMontage> AttackMontage;
-
-	// 히트 성공 시 대상에 적용할 데미지 GE (GE_SHBossMeleeDamage).
-	UPROPERTY(EditDefaultsOnly, Category = "SH|Boss|Melee")
-	TSubclassOf<UGameplayEffect> DamageEffect;
+	//~ USHMeleeAttackBase 타입별 훅
+	virtual FGameplayTag GetHitDetectTag() const override;
+	virtual bool ComputeHitTrace(const FGameplayAbilityActorInfo* ActorInfo,
+		FVector& OutStart, FVector& OutEnd, float& OutRadius) const override;
+	//~ End of USHMeleeAttackBase 타입별 훅
 
 	// 히트 판정 중심이 될 스켈레탈 메시 소켓 이름.
 	// 소켓이 없으면 캐릭터 전방 구체 트레이스로 폴백.
@@ -92,7 +46,4 @@ protected:
 	// 소켓이 없을 때 전방 트레이스 거리 (cm).
 	UPROPERTY(EditDefaultsOnly, Category = "SH|Boss|Melee", meta = (ClampMin = "1.0"))
 	float FallbackRange = 200.0f;
-
-	UPROPERTY(EditDefaultsOnly, Category = "SH|Boss|Melee|Debug")
-	bool bDrawDebugTrace = false;
 };
